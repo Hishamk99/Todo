bc43897a867b79fca1cdfaa00825df4b2da41666
Hishamk99 <hishamkamel370@email.com>
2024-04-14 11:09:34 +0200

    create task model

---
+++ lib/model/task_model.dart
@@ -0,0 +1,15 @@
+class TaskModel {
+  final String title;
+  final String description;
+
+  TaskModel({
+    required this.title,
+    required this.description,
+  });
+
+  // factory TaskModel.fronJson(jsonData) {
+  //   return TaskModel(title: ,
+  //    description:
+  //   );
+  // }
+}
